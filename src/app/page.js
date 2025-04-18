e737224e8718d7480aefe3d9bf15289efe9fd051
David_master26 <lelouch45v@gmail.com>
2025-04-19 01:44:39 +0800

    adding dark theme and adding ui in the shadCN

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
       <div>
           <h1>
             this is pages
+
+            <Button 
+              variant="outline"
+            >
+
+              login button
+
+            </Button>
           </h1>
       </div>
   );
